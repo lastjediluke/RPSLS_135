b12d6591b6b9d90953e97d2db0cca9568b174e7d
dkits <derickL12@yahoo.com>
2019-02-28 00:19:14 -0800

    I'm a bit unsure about how this will interact with the other classes.  Requires more discussion.

---
+++ MainMenu.cpp
@@ -0,0 +1,24 @@
+#include "MainMenu.h"
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+
+
+void Menu::MainMenu(){
+	string x;
+	do{ 
+	cout << "Welcome to Rock, Paper Scissor, Lizard, Spock!" << endl;
+	cout << "1.) Start Game "     << endl; 
+	cout << "2.) User Management" << endl;
+	cout << "3.) Leaderboards"    << endl;
+	cout << "4.) Settings"        << endl; 
+	cout << "5.) Quit"            << endl;
+    getline(cin, x);
+    if(x.compare("1")){
+    	cout << "Hello" << endl;
+    }
+	}
+	while(x.compare("5"));
+}
---
+++ MainMenu.h
@@ -0,0 +1,12 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+
+class Menu 
+{
+public:
+	void MainMenu(); 
+private:
+};
+
+#endif // MainMenu.h

70cc2be6887b7b1eb977aca2960b3d4214de4148
Yosef <ymmirsky@gmail.com>
2019-03-01 13:55:10 -0800

    Moved the instruction printout to its own function. Renamed setRoundCount() to incRoundCount(), set a const parameter for gameTime

--- GameInstance.h
+++ GameInstance.h
@@ -13,18 +13,20 @@ class GameInstance
 public:
     GameInstance();
     void startGameLoop();
+    void printInstructions();
     bool isValidHand(char key);
     void updateScore(Hands::handType winner);
     int getRoundCount()     { return roundCount;    }
     int getGameCount()      { return gameCount;     }
-    int setRoundCount()     { return roundCount++;  }
-    int setGameCount()      { return gameCount++;   }
+    int incRoundCount()     { return roundCount++;  }
+    int incGameCount()      { return gameCount++;   }
     int resetRoundCount()   { roundCount = 0; return roundCount; }
 private:
     ComputerPlayer *cpu;
     HumanPlayer *myPlayer;
     int roundCount;
     int gameCount;
+    const int gameTime = 21;
 };
 
 
